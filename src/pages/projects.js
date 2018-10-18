5f648b58af088ace4b8cc76c930e3bd3a32a4504
Michael Ganzorig <manlai1025@gmail.com>
2018-10-17 21:37:41 -0700

    reverting back

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -48,6 +48,45 @@ var projects = [
   },
 ];
 
+export class ProjectCard extends Component
+{
+  render()
+  {
+    return (
+      <Grid item>
+        <Paper style={{ textAlign: 'center',
+          maxWidth: 340,
+          marginBottom: 20}} id="card-projects">
+        <CardMedia style = {{ height: 0,
+          paddingTop: '56%',
+          objectFit: 'cover',
+          marginBottom: 0,
+          paddingBottom: 0,}}
+          image={this.props.image}/>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h5">
+                        {this.props.title}
+                    </Typography>
+                    <Typography component="p">
+                        {this.props.description}
+                    </Typography>
+                </CardContent>
+            <CardActions>
+                <Button size="small" color="primary" href={this.props.more} target="_blank">
+                  Learn More
+                </Button>
+                <IconButton size="small" color="primary" href={this.props.link} target="_blank">
+                  <SvgIcon>
+                    <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
+                  </SvgIcon>
+                </IconButton>
+            </CardActions>
+        </Paper>
+      </Grid>
+    );
+  }
+}
+
 export default class GridProjects extends Component
 {
   render()
@@ -57,39 +96,12 @@ export default class GridProjects extends Component
         <h1>Projects</h1>
         <div id="quote"><i>"First, solve the problem. Then, write the code." - John Johnson</i></div>
         <Grid container justify="center" spacing="40">
-        {projects.map(proj => {
-          return ( 
-          <Grid item>
-          <Paper style={{ textAlign: 'center',
-            maxWidth: 340,
-            marginBottom: 20}} id="card-projects">
-          <CardMedia style = {{ height: 0,
-            paddingTop: '56%',
-            objectFit: 'cover',
-            marginBottom: 0,
-            paddingBottom: 0,}}
-            image={proj.image}/>
-                  <CardContent>
-                      <Typography gutterBottom variant="h5" component="h5">
-                          {proj.title}
-                      </Typography>
-                      <Typography component="p">
-                          {proj.description}
-                      </Typography>
-                  </CardContent>
-              <CardActions>
-                  <Button size="small" color="primary" href={proj.more} target="_blank">
-                    Learn More
-                  </Button>
-                  <IconButton size="small" color="primary" href={proj.link} target="_blank">
-                    <SvgIcon>
-                      <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
-                    </SvgIcon>
-                  </IconButton>
-              </CardActions>
-          </Paper>
-        </Grid>
-        )})}
+        {projects.map(proj => { 
+          return (<ProjectCard
+                  title={proj.title} image={proj.image}
+                  description={proj.description}
+                  link={proj.link} more={proj.more}/>);
+          })}
         </Grid>
       </Layout>
     );
